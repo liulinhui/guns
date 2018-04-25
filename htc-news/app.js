c4510cb7841d0f89ff449003d9e3f5e59e303f90
liulinhui <1249055292@qq.com>
2018-04-25 16:53:57 +0800

    pass

--- htc-news/app.js
+++ htc-news/app.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'healthTrace@12345',
     database: 'guns'
 });
 connection.connect();
