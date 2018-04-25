372906a381c3d3881ddb4fe121ce20d296f64799
liulinhui <1249055292@qq.com>
2018-04-25 17:57:19 +0800

    add auto run script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 git pull
-mvn clean package
 
 #默认8080端口
 port=8080
@@ -9,10 +8,11 @@ if [ "$1" ];then
     port="$1"
 fi
 
-#关闭8080端口
-kill -9 $(lsof -i:${port} |awk '{print $2}' | tail -n 2)
 
 mvn clean package -Dmaven.test.skip=true
+
+#关闭8080端口
+kill -9 $(lsof -i:${port} |awk '{print $2}' | tail -n 2)
 
 cd target/
 java -Duser.timezone=GMT+8 -jar sg-server.jar --spring.profiles.active=produce --server.port=${port}
