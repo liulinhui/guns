a54666cd6970f34baa16be19ba469440db0048e3
liulinhui <1249055292@qq.com>
2018-04-25 17:36:34 +0800

    app cros

--- htc-news/app.js
+++ htc-news/app.js
@@ -15,7 +15,15 @@ var connection = mysql.createConnection({
 connection.connect();
 
 var app = express();
-
+// 跨域设置
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next()
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
