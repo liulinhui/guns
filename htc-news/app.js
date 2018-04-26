aa566ea002d613bb3b672af695cbb515db390bee
liulinhui <1249055292@qq.com>
2018-04-26 15:43:46 +0800

    bug

--- htc-news/app.js
+++ htc-news/app.js
@@ -41,7 +41,7 @@ app.get('/newsInterface/list', function (req, res) {
     var page = req.query.page || 1;
     var limit = req.query.limit || 10;
     var offset = (page - 1) * limit;
-    connection.query(`select id,title,draft,time from news where language='${language}' order by time desc limit ${offset},${limit};`, function (err, result, fileds) {
+    connection.query(`select id,title,draft,realtime,time from news where language='${language}' order by time desc limit ${offset},${limit};`, function (err, result, fileds) {
         if (err) res.send(err);
         else {
             data.data = result;
